e2df5d0be743635179b25ea067a3e4ca304ffe2e
Yeci <shawnyin226@gmail.com>
2026-02-04 21:49:52 +0800

    fix: remove deprecated --ignore-path flag from eslint command

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,9 @@ import globals from 'globals'
 import pluginJs from '@eslint/js'
 
 export default [
+  {
+    ignores: ['docs/**', 'dist/**', 'node_modules/**', 'coverage/**'],
+  },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
 ]
